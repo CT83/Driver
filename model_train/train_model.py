6e2663ddbf315b8db1255fc7cabccd7319f72e4a
Rohan Sawant <rohansawantct83@gmail.com>
2018-04-21 18:10:10 +0530

    Load npy array from all of data at once

--- model_train/train_model.py
+++ model_train/train_model.py
@@ -7,12 +7,12 @@ from alexnet import alexnet
 WIDTH = 50
 HEIGHT = 50
 LR = 1e-3
-EPOCHS = 25
-MODEL_NAME = 'April_20_Model_4'
+EPOCHS = 20
+MODEL_NAME = 'April_21_Model_1'
 
-PREV_MODEL = 'model_alexnet-788'
+PREV_MODEL = 'model_alexnet-816'
 
-LOAD_MODEL = False
+LOAD_MODEL = True
 
 model = alexnet(WIDTH, HEIGHT, LR)
 if LOAD_MODEL:
@@ -50,9 +50,21 @@ def balance_data(train_data):
 
 hm_data = 5
 for i in range(EPOCHS):
-    for i in range(1, hm_data + 1):
-        train_data = np.load('D:/Training Data/Driver/50_50 Long/training_data-{}.npy'.format(i))
+    for i in range(1, hm_data + 1, 10):
 
+        train_data = []
+
+        for j in range(i, i + 10):
+            try:
+                print("Loading 50_50 Long/training_data-{}.npy", j)
+                inf_from_every_file = np.load('D:/Training Data/Driver/50_50 Long/training_data-{}.npy'.format(j))
+                train_data.append(inf_from_every_file)
+            except Exception as e:
+                print(e)
+                print('Failted to Load training_data-{}.npy', j)
+
+        train_data = np.concatenate(train_data)
+        print("Training Data ", str(len(train_data)), " Data ", str(train_data))
         train_data = balance_data(train_data)
         train = train_data[:-100]
         test = train_data[-100:]
@@ -69,4 +81,4 @@ for i in range(EPOCHS):
         print("Model Saved", i)
         model.save(MODEL_NAME)
 
-# tensorboard --logdir=foo:C:/
+        # tensorboard --logdir=foo:C:/
